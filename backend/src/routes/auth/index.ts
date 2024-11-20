01f4227c18c787a48c784b3d347a79550e170b8d
Rasha Moumneh <rasha@rasha.dev>
2024-11-20 18:30:39 -0500

    Auth: implement user registration flow with token generation

--- backend/src/routes/auth/index.ts
+++ backend/src/routes/auth/index.ts
@@ -1,23 +1,72 @@
 import { Router, Request, Response } from "express";
 import { asyncHandler } from "@/routes/middleware/errorHandler";
+import isEmail from "validator/es/lib/isEmail";
+import { User } from "@/models/user.model";
+import { tokenUtils } from "@/utils/token.utils";
+import { jwtConfig } from "@/config/jwt.config";
 import {
   accessTokenCookieConfig,
   refreshTokenCookieConfig,
 } from "@/config/cookie.config";
+import logger from "@/logger";
+import { AuthenticationError } from "@/errors/auth.errors";
+import { ValidationError } from "@/errors/base.errors";
 
 const router = Router();
 
 router.post(
   "/register",
   asyncHandler(async (req: Request, res: Response) => {
-    // 1. validate input
-    // 2. check if user exists in db
-    // 3. hash password using bcrypt/argon2
-    // 4. create new user record
-    // 5. generate initial access and refresh tokens
-    // 6. store refresh token with user in DB
-    // 7. set httpOnly cookies with tokens
-    // 8. return success response with user info (excluding sensitive data)
+    // Removed NextFunction since we're not using it
+    if (!req.body.email || !req.body.password) {
+      throw new ValidationError("Email and password are required", 400);
+    }
+    // validate email
+    const isValidEmail = isEmail(req.body.email);
+    if (!isValidEmail) throw new ValidationError("Invalid email format", 400);
+
+    // validate password
+    if (req.body.password.length < 8) {
+      throw new ValidationError("Password must be at least 8 characters", 400);
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: req.body.email.toLowerCase() });
+    if (user) {
+      throw new ValidationError("User already exists. Please sign in", 400);
+    }
+
+    // create new user, tokens, and set cookies
+    const newUser = await User.create({
+      email: req.body.email.toLowerCase(),
+      password: req.body.password.trim(),
+    });
+
+    const newAccessToken = tokenUtils.generateAccessToken(newUser.id);
+    const { token, family, version } = tokenUtils.generateRefreshToken(
+      newUser.id,
+    );
+
+    newUser.refreshTokens.push({
+      family,
+      version,
+      expiresAt: new Date(Date.now() + jwtConfig.refreshToken.expiresInMs),
+      userId: newUser.id,
+    });
+
+    await newUser.save();
+
+    res.cookie("accessToken", newAccessToken, accessTokenCookieConfig);
+    res.cookie("refreshToken", token, refreshTokenCookieConfig);
+
+    return res.status(201).json({
+      user: {
+        id: newUser.id,
+        email: newUser.email,
+        createdAt: newUser.createdAt,
+      },
+      message: "New user created",
+    });
   }),
 );
 
