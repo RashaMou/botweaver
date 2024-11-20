9fce9556236216caf64bffdbbcc525a616d38c8a
Rasha Moumneh <rasha@rasha.dev>
2024-11-20 18:45:27 -0500

    Auth: add authentication middleware to verify access tokens

--- backend/src/routes/middleware/authenticate.ts
+++ backend/src/routes/middleware/authenticate.ts
@@ -1,5 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import { AuthenticationError } from "@/errors/auth.errors";
 import jwt from "jsonwebtoken";
+
+interface UserPayload {
+  id: string;
+  email: string;
+  createdAt: Date;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: UserPayload;
+    }
+  }
+}
 
 export const authenticate = (
   req: Request,
@@ -10,6 +25,18 @@ export const authenticate = (
   // 2. Verify the token
   // 3. If valid, attach user info to req object
   // 4. If invalid, return 401 unauthorized
+  try {
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) throw new AuthenticationError("Not authenticated", 401);
 
-  next();
+    const payload = jwt.verify(
+      accessToken,
+      process.env.JWT_SECRET!,
+    ) as UserPayload;
+
+    req.user = payload;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
