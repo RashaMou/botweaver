2dd2e5cdf216a234f21391700c5eb7a8e3868703
Rasha Moumneh <rasha@rasha.dev>
2024-11-20 19:26:38 -0500

    Bugfix: update router export and errorhandling
    - Removed ErrorHandler from router (it's already in the middleware)
    - Corrected router export

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import apiRoutes from "./api";
 import authRoutes from "./auth";
-import { errorHandler } from "./middleware/errorHandler";
 import { authenticate } from "./middleware/authenticate";
 
 const router = Router();
 
 router.use("/api", authenticate, apiRoutes);
 router.use("/auth", authRoutes);
-router.use(errorHandler);
 
-export default Router;
+export default router;
