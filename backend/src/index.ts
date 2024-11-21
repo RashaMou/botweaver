e3b43459bb02b1d1c12babd05e0ba2fd49d8ecae
Rasha Moumneh <rasha@rasha.dev>
2024-11-20 19:25:06 -0500

    refactor: add dotenv package
    Added dotenv package and made sure server code accesses it

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,17 @@
 import express, { Express } from "express";
+import * as dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import logger from "@/logger";
 import routes from "@/routes";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "@/routes/middleware/errorHandler";
+import connectDB from "@/config/db";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, "../.env") });
 
 const app: Express = express();
 const port: number = parseInt(process.env.PORT || "8080", 10);
